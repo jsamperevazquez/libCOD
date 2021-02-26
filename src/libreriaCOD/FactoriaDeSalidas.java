5bb3761f46236f626394aad3f8c8630b9e8ba835
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-02-26 10:04:49 +0100

    Modificada FactoriaDeSalidas para impresora

--- src/libreriaCOD/FactoriaDeSalidas.java
+++ src/libreriaCOD/FactoriaDeSalidas.java
@@ -5,15 +5,18 @@ package libreriaCOD;
  * El  26 de feb. de 2021.
  **/
 public class FactoriaDeSalidas {
-    public static final int CONSOLA=1;
-    public static final int VENTANA=2;
+    public static final int CONSOLA = 1;
+    public static final int VENTANA = 2;
+    public static final int IMPRESORA = 3;
 
-    public static InterfaceMetodosComunes getProducto(int tipo){
-        switch (tipo){
+    public static InterfaceMetodosComunes getProducto(int tipo) {
+        switch (tipo) {
             case CONSOLA:
                 return new Consola();
             case VENTANA:
                 return new Ventana();
+            case IMPRESORA:
+                return new Impresora();
             default:
                 return null;
         }
