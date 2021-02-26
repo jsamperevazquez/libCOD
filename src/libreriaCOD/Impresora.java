ca84e6bf5fc944543186278ce60fa1a849551721
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-02-26 10:01:59 +0100

    Añadida clase impresora

---
+++ src/libreriaCOD/Impresora.java
@@ -0,0 +1,12 @@
+package libreriaCOD;
+
+/**
+ * Creado por @autor: angel
+ * El  26 de feb. de 2021.
+ **/
+public class Impresora  implements InterfaceMetodosComunes{
+    @Override
+    public void visualizar(String mensaje) {
+
+    }
+}
