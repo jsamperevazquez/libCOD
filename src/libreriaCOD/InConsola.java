a7b79b5279a4c40f35f327255288f52d75454320
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-03-02 11:22:08 +0100

    Modificadas las factorías como entrada y salida

--- src/libreriaCOD/InConsola.java
+++ src/libreriaCOD/InConsola.java
@@ -16,7 +16,7 @@ public class InConsola implements InterfaceMetodosComunes{
 
     @Override
     public void introducirDatos() {
-        System.out.println("Por favor, introduzca datos");
+        System.out.println("Por favor, introduzca datos\n");
         Scanner sc = new Scanner(System.in);
         String entrada="";
         entrada=sc.nextLine();
